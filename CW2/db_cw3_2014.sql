9bee13c1b2ba090f23d0c2c5072ffbbc953a8c30
HoldenCaulfieldICL <adalyac@gmail.com>
2014-03-06 22:18:49 +0000

    about to try different q5 approach

--- CW2/db_cw3_2014.sql
+++ CW2/db_cw3_2014.sql
@@ -21,18 +21,60 @@ ORDER BY popularity DESC;
 
 -- Q3 returns (house,seventeenth,eighteenth,nineteenth,twentieth)
 SELECT   house,
-	 COUNT(CASE WHEN accession>='1600-01-01' and accession<'1700-01-01' THEN 1 ELSE NULL END) AS seventeenth,
-	 COUNT(CASE WHEN accession>='1700-01-01' and accession<'1800-01-01' THEN 1 ELSE NULL END) AS eighteenth,
-	 COUNT(CASE WHEN accession>='1800-01-01' and accession<'1900-01-01' THEN 1 ELSE NULL END) AS nineteenth,
-	 COUNT(CASE WHEN accession>='1900-01-01' and accession<'2000-01-01' THEN 1 ELSE NULL END) AS twentieth
+	 COUNT(CASE WHEN accession>='1600-01-01' AND accession<'1700-01-01' THEN 1 ELSE NULL END) AS seventeenth,
+	 COUNT(CASE WHEN accession>='1700-01-01' AND accession<'1800-01-01' THEN 1 ELSE NULL END) AS eighteenth,
+	 COUNT(CASE WHEN accession>='1800-01-01' AND accession<'1900-01-01' THEN 1 ELSE NULL END) AS nineteenth,
+	 COUNT(CASE WHEN accession>='1900-01-01' AND accession<'2000-01-01' THEN 1 ELSE NULL END) AS twentieth
 FROM     monarch
 GROUP BY house
 ORDER BY house;
 
 
 -- Q4 returns (name,age)
-SELECT   name,
-	 AS age,
-FROM     person as parent
-	 JOIN person
-ORDER BY name
+SELECT   parent.name,
+	 MIN(child.dob) AS first_child_dob
+FROM     person AS child
+	 JOIN person AS parent
+	 ON child.father = parent.name OR child.mother = parent.name
+GROUP BY parent.name
+ORDER BY parent.name;
+
+
+-- use this to double check
+-- SELECT   parent.name AS parent_name,
+-- 	 child.name AS child_name,
+-- 	 child.dob AS first_dob
+-- FROM     person AS child
+-- 	 JOIN person AS parent
+-- 	 ON child.father = parent.name OR child.mother = parent.name
+-- ORDER BY parent.name;
+
+
+-- Q5 returns (father,child,born)
+SELECT   parent.name AS father,
+ 	 child.name AS child,
+ 	 COUNT(CASE 
+	       WHEN child.dob < otherchild.dob 
+	            THEN 1 
+	            ELSE 0
+	       END) 
+	       OVER (PARTITION BY parent.name) AS born
+FROM     person AS parent
+ 	 JOIN person AS child
+ 	 ON child.father = parent.name
+ 	 JOIN person AS otherchild
+ 	 ON child.father = otherchild.name
+ORDER BY parent.name;
+
+
+
+SELECT   parent.name AS father,
+	 child.name AS child,
+	 OVER (PARTITION BY father) AS born
+FROM     person
+WHERE    gender = 'M'
+ORDER BY name;
+
+
+
+
